77736b9f4c1b58e551f95e056649782941501131
Shereemorphett <sheree.morphett@gmail.com>
2024-09-01 19:32:23 +0300

    classes set up but not created

--- src/ro/academyplus/avaj/simulator/Simulator.java
+++ src/ro/academyplus/avaj/simulator/Simulator.java
@@ -2,10 +2,34 @@ package ro.academyplus.avaj.simulator;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import ro.academyplus.avaj.AircraftFactory;
 
+/*
+	private static void loadAircrafts() throws SimulationException {
+		try {
+			String info[];
+			tower = new WeatherTower();
+			while ((currentLine = br.readLine()) != null) {
+				info = currentLine.split("\\s+");
+				AircraftFactory.newAircraft(info[0], info[1], Integer.parseInt(info[2]),
+					Integer.parseInt(info[3]), Integer.parseInt(info[4])).registerTower(tower);
+			}
+			br.close();
+		} catch (IOException e) {
+			throw new SimulationException(e);
+		}
+	}
+	}
+}*/
 public class Simulator {
+    
+    private static int cyclesCount;
+	private static BufferedReader br;
+	private static String data;
+    private static WeatherTower tower;
+    
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java ro.academyplus.avaj.simulator.Simulator <scenario-file>");
@@ -14,26 +38,28 @@ public class Simulator {
 
         try
         {
-        String scenarioFile = args[0];
-        File file = new File(scenarioFile);
-        Scanner scenario_input = new Scanner(file);
+            //would be in an init/load aircrafts, ini cycleCount
+            String scenarioFile = args[0];
+            File file = new File(scenarioFile);
+            Scanner scenario_input = new Scanner(file);
 
-        if (!file.exists()) {
-            System.out.println("File not found: " + scenarioFile);
-            return;
-        }
-        while (scenario_input.hasNextLine())
-        {
-            String data = scenario_input.nextLine();
-            System.out.println(data);
-        }
-      scenario_input.close();
-
+            if (!file.exists()) {
+                System.out.println("File not found: " + scenarioFile);
+                return;
+            }
+            while (scenario_input.hasNextLine())
+            {
+                data = scenario_input.nextLine();
+                System.out.println(data);
+            }
+            
+            scenario_input.close();
             System.out.println("Processing scenario file: " + scenarioFile);
             System.out.println("Processing scenario file: " + file);
 
-
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.err.println("An error occurred while reading the file: " + e.getMessage());
             e.printStackTrace();
         }
